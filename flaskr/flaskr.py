81f5ec4406069697d8172b19468512f5d75b9ca7
atardecer <atardecer@qq.com>
2017-11-03 10:32:47 +0800

    version_0.1

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -6,25 +6,26 @@ from flask import Flask, request, session, g,\
 def create_db():
 	conn = sqlite3.connect('flaskr.db')
 	c = conn.cursor()
-	c.execute('CREATE TABLE content(id INT(3) PRIMARY KEY, title VARCHAR(20) NOT NULL,\
-					txt VARCHAR(2000)) NOT NULL')
+	c.execute('CREATE TABLE content(id INT(3) PRIMARY KEY, title VARCHAR(20),text VARCHAR(2000))')
+	c.execute('CREATE TABLE user(username VARCHAR(20), password VARCHAR(20))')
+	c.execute('INSERT INTO user VALUES(\'admin\', \'password\')')
 	c.close()
 	conn.commit()
 	conn.close()
-d
+
 
 
 app = Flask(__name__)
 
-@app.route('/', method=['GET'])
+@app.route('/', methods=['GET'])
 def login():
 	return render_template('login.html')
 
-@app.route('/', method=['POST'])
+@app.route('/', methods=['POST'])
 def login_info():
 	username = request.form['username']
 	password = request.form['password']
-	error = None
+	error = 'bad username or password'
 
 	if username and password:
 		conn = sqlite3.connect('flaskr.db')
@@ -33,24 +34,23 @@ def login_info():
 		rel_password = c.execute('SELECT password FROM user')[0][0]
 		if username == rel_username and password == rel_password:
 			return redirect(url_for(index, '/blog'))
-		else: error = 'bad username or password '
 	
 	return render_template('login.html', error=error)			
 
 
-@app.route('/blog', method=['GET'])
+@app.route('/blog', methods=['GET'])
 def index():
 	return render_template('index.html')
 
-@app.route('/blog', method=['POST'])
+@app.route('/blog', methods=['POST'])
 def submit():
 	title = request.form['title']
-	text = request.form['text']
-	
+	text = request.form['text']	
+	error = 'title or text can not be empty! '
 	if title and text:
 		conn = sqlite3.connect('flaskr.db')
 		c = conn.cursor()
-		the_id =(c.execute('SELECT MAX(id) FROM content')][0][0])
+		the_id =(c.execute('SELECT MAX(id) FROM content')[0][0])
 		if the_id:
 			the_id = int(the_id) + 1
 		else:the_id = 1
@@ -58,15 +58,12 @@ def submit():
 		c.close()
 		conn.commit()
 		conn.close()
-	return render.template('index.html', error='submit successful!')
+		return render.template('index.html', error='submit successful!')
 
-	else: 
-		error = 'title or text can not be empty! '
-		return render_template('index.html', error=error)
+	return render_template('index.html', error=error)
 
 
 
 
 if __name__ == '__main__':
-	app.run()
-
+	 app.run(debug=True) 
